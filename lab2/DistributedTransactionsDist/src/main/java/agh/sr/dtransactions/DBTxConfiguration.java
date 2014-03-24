34aa251b5eea60ce43b5f6171ae1838109f8c2d9
turu <piotr.turu.turek@gmail.com>
2014-03-24 23:42:29 +0100

    lab2 os fix

--- lab2/DistributedTransactionsDist/src/main/java/agh/sr/dtransactions/DBTxConfiguration.java
+++ lab2/DistributedTransactionsDist/src/main/java/agh/sr/dtransactions/DBTxConfiguration.java
@@ -43,6 +43,8 @@ public class DBTxConfiguration {
 
 	private static final int NUMBER_OF_USERS = 10;
 
+	private static String OS = System.getProperty("os.name").toLowerCase();
+
 	private static String[][] CUSTOMERS = new String[][] {
 			{ "Marek", "Kowalski", "Krakow" }, { "Jacek", "Nowak", "Wroclaw" },
 			{ "Maciek", "Zielinski", "Poznan" },
@@ -69,12 +71,45 @@ public class DBTxConfiguration {
 		}
 	}
 
+	public static boolean isWindows() {
+
+		return (OS.indexOf("win") >= 0);
+
+	}
+
+	public static boolean isMac() {
+
+		return (OS.indexOf("mac") >= 0);
+
+	}
+
+	public static boolean isUnix() {
+
+		return (OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS
+				.indexOf("aix") > 0);
+
+	}
+
+	public static boolean isSolaris() {
+
+		return (OS.indexOf("sunos") >= 0);
+
+	}
+
 	private static Connection ensureThatDBisCreated(String name,
 			String dbSchemaFile) throws Exception {
 		String dbUrl = DERBY_URL_PREFIX + name;
 		File dbDirectory = new File(name);
 		if (!dbDirectory.exists()) {
-			Runtime.getRuntime().exec("cmd.exe /c rmdir /q /s " + name);
+			if (isWindows()) {
+				Runtime.getRuntime().exec("cmd.exe /c rmdir /q /s " + name);
+			} else if (isUnix()) {
+				Runtime.getRuntime().exec("rm " + name);
+			}
+			else {
+				throw new RuntimeException("Invalid os");
+			}
+
 			Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
 			Connection dbConnection = DriverManager.getConnection(dbUrl
 					+ ";create=true");
